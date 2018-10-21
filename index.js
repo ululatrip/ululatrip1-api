1eb1c638b510c0506f43cc1bb87dc91756f81265
Regi Arfiandi <regiarfiandi@Smile.local>
2018-10-21 18:32:44 +0700

    add api

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("helowwwwwwwww")
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Client } = require("pg");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/trip", async (req, res) => {
+  try {
+    const trip = await client.query("SELECT * FROM trip;");
+    res.header("Content-Type", "application/json");
+    res.status(200);
+    return res.json({ data: trip.rows });
+  } catch (e) {
+    console.error(e);
+    res.status(500);
+    return res.json({ error: "Internal server error" });
+  }
+});
+
+app.all("*", (req, res) => {
+  return res.json({ data: "Hello world!" });
+});
+
+process.env.PORT = process.env.PORT || 3000;
+const port = process.env.PORT;
+module.exports = app.listen(port, () => {
+  console.log(`Backend server started`);
+});
